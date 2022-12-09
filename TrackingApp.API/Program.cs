a11da767c974b3801d204931ecda4b831d5c509c
menasamy <mena.samy@gmail.com>
2022-12-09 22:48:23 +0200

    initial commit

---
+++ TrackingApp.API/Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TrackingApp.Core.Entities;
+using TrackingApp.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddDbContext<TAContext>(options => options.UseSqlServer(configuration.GetConnectionString("ConnStr")));
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<TAContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
+app.MapControllers();
+
+app.Run();
---
+++ TrackingApp.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,161 @@
+﻿using TrackingApp.Core.Interface;
+using TrackingApp.Core.Shared;
+using TrackingApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSystem.Infrastructure.Services
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        private readonly TAContext _dbContext;
+
+        public Repository(TAContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Insert(T entity)
+        {
+            entity.CreatedDate = DateTime.UtcNow;
+            _dbContext.Set<T>().Add(entity);
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                _dbContext.Entry(entity).State = EntityState.Detached;
+                throw ex;
+            }
+            
+        }
+
+        public async Task<int> BulkInsert(List<T> entity)
+        {
+            entity.All(a => { a.CreatedDate = DateTime.UtcNow; return true; });
+            _dbContext.Set<T>().AddRange(entity);
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                _dbContext.Entry(entity).State = EntityState.Detached;
+                throw ex;
+            }
+            
+        }
+        public async Task<int> BulkDelete(List<T> entity)
+        {
+            entity.All(a => { a.ModifiedDate = DateTime.UtcNow; a.IsDeleted = true; return true; });
+            _dbContext.Set<T>().UpdateRange(entity);
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                _dbContext.Entry(entity).State = EntityState.Unchanged;
+                throw ex;
+            }
+        }
+        public async Task<int> Delete(T entity)
+        {
+            // soft delete
+            entity.ModifiedDate = DateTime.UtcNow;
+            entity.IsDeleted = true;
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                _dbContext.Entry(entity).State = EntityState.Unchanged;
+                throw ex;
+            }
+            
+        }
+
+        public async Task<int> HardDelete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                _dbContext.Entry(entity).State = EntityState.Unchanged;
+                throw ex;
+            }
+        }
+
+        public async Task<int> Update(T entity)
+        {
+            entity.ModifiedDate = DateTime.UtcNow;
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                _dbContext.Entry(entity).State = EntityState.Unchanged;
+                throw ex;
+            }
+        }
+
+        public async virtual Task<T> GetById(int id, string include)
+        {
+            var entity = _dbContext.Set<T>().Where(a => a.Id == id && (a.IsDeleted != true || a.IsDeleted == null));
+            if (!string.IsNullOrEmpty(include))
+                entity.Include(include);
+            return await entity.FirstOrDefaultAsync();
+        }
+
+        public async virtual Task<IEnumerable<T>> GetAll()
+        {
+            return _dbContext.Set<T>().Where(a => a.IsDeleted != true || a.IsDeleted == null)
+                //.Include(a => a.CreatedByName)
+                //.Include(a => a.ModifiedByName)                
+                .AsEnumerable();
+        }
+
+        public async Task<IEnumerable<T>> Get(Expression<Func<T, bool>> predicate, string include)
+        {
+            var list = _dbContext.Set<T>()
+               .Where(predicate)
+               .Where(a => a.IsDeleted != true || a.IsDeleted == null);
+            if (!string.IsNullOrEmpty(include))
+                list = list.Include(include);
+
+            return list.AsEnumerable();
+        }
+
+        public async Task<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>()
+               .Where(predicate)
+               .Where(a => a.IsDeleted != true || a.IsDeleted == null)
+               .FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return (await GetById(id, null) != null);
+        }
+
+        private void HandleSavingExceptions(List<T> entries)
+        {
+           
+        }
+
+    }
+}
